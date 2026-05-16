e1275edd7059e590cacc4d7b71f61f1c8839e877
achita <achita.p@kkumail.com>
2026-05-16 18:50:02 +0700

    fix

--- src/api/base44Client.js
+++
@@ -1,8 +0,0 @@
-import { Base44 } from '@base44/sdk';
-import { appParams } from '@/lib/app-params';
-
-export const base44 = new Base44({
-  appId: appParams.appId,
-  token: appParams.token,
-  appBaseUrl: appParams.appBaseUrl || 'https://api.base44.com'
-});
